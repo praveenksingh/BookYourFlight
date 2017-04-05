2172940cd028f2fca52bab7fe65e2553f3e7407f
praveensingh <praveen@Praveens-MacBook-Pro.local>
2017-04-05 16:37:16 -0400

    updated code

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use(passport.session());
 
 global.apiPath = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=API_KEY";
 // global.key1 = process.env.GOOGLE_KEY ||"AIzaSyDKbf5xMjHgx2AxbT8XYiemow5DPfBEj0I";
-global.key1 = process.env.GOOGLE_KEY ||"AIzaSyBviYdw6Yqic5WxRZ21KjVgQqXOKMWG7GI";
-var key = process.env.GOOGLE_KEY ||"AIzaSyB0Qaqmq90cU-511qA7AterDFbNrMvUwtU";
+global.key1 = process.env.GOOGLE_KEY_PLACES ||"AIzaSyBviYdw6Yqic5WxRZ21KjVgQqXOKMWG7GI";
+var key = process.env.GOOGLE_KEY_FLIGHT ||"AIzaSyB0Qaqmq90cU-511qA7AterDFbNrMvUwtU";
 apiPath = apiPath.replace("API_KEY", key);
 
 
