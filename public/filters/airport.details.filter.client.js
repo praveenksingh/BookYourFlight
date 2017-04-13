9ee89339f66317f4fbd61475c487f868394b0540
praveensingh <praveen@Praveens-MacBook-Pro.local>
2017-04-13 16:51:30 -0400

    updates

--- public/filters/airport.details.filter.client.js
+++ public/filters/airport.details.filter.client.js
@@ -4,26 +4,15 @@
         .controller("AirportDetails", airportLoaded);
 
     function airportLoaded($scope, AirportService) {
-        var vm = this;
-        vm.render = render;
 
         function init() {
-            // console.log($scope.comment);
-            AirportService
-                .findAirportById($scope.comment)
-                .then(function (airport) {
-                    $scope.airport = airport;
-                })
+            // AirportService
+            //     .findAirportById($scope.comment)
+            //     .then(function (airport) {
+            //         $scope.airport = airport;
+            //     })
         }
         init();
-
-        function render(){
-            AirportService
-                .findAirportById(vm.comment)
-                .then(function (airport) {
-                    vm.airport = airport;
-                })
-        }
 
     }
 })();
